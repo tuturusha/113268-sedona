638c734457ad6b7eb0e012de98ae3c097b36bcaa
Yulia Yakovleva <yulya055@mail.ru>
2017-12-05 23:31:25 +1000

    Add animation

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var button=document.querySelector(".button-search");
+
+var form=document.querySelector(".offer-form");
+
+button.addEventListener("click", function (evt) {
+    evt.preventDefault();
+  });
+
+form.classList.remove("form-show");
+
+button.addEventListener("click", function() {
+    form.classList.toggle("form-show");
+});
